76a2414473d3f52fb973d6b592bb3a512c2b6448
JosueSifuentesA <contactojosuesifuentes@gmail.com>
2023-07-04 20:50:38 -0500

    Added gitignore

--- restaurant/Program.cs
+++ restaurant/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("PostgreSQLConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
